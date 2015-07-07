03c7c6cd26c179521f0bd0aa3e658d403c710ac1
mustafa220 <musto_220@windowslive.com>
2015-07-07 21:27:00 +0300

    ogretmenEkle_view.php

--- yonetim/apps/view/ogretmenEkle_view.php
+++ yonetim/apps/view/ogretmenEkle_view.php
@@ -1,3 +1,10 @@
 <div class="sag">
-	aaa
+	<form action="" method="post">
+		<table class="formTablo">
+			<tr>
+				<td class="bilgi">Öğretmen Adı</td>
+				<td class="input"><input type="text" name="ogretmenAdi" /></td>
+			</tr>
+		</table>
+	</form>
 </div>
