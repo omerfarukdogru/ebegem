817246295e43cbd770fa924565f038cb90aeaade
mustafa220 <musto_220@windowslive.com>
2015-07-07 21:30:48 +0300

    aaa

--- yonetim/apps/view/ogretmenEkle_view.php
+++ yonetim/apps/view/ogretmenEkle_view.php
@@ -1,9 +1,21 @@
 <div class="sag">
-	<form action="" method="post">
+	<form action="" method="post" enctype="multipart/form-data">
 		<table class="formTablo">
 			<tr>
 				<td class="bilgi">Öğretmen Adı</td>
 				<td class="input"><input type="text" name="ogretmenAdi" /></td>
+			</tr>
+			<tr>
+				<td class="bilgi">Öğretmen Bilgileri</td>
+				<td class="input"><textarea name="ogretmenBilgileri" cols="30" rows="10"></textarea></td>
+			</tr>
+			<tr>
+				<td class="bilgi">Öğretmen Resmi</td>
+				<td class="input"><input type="file" name="resim" /></td>
+			</tr>
+			<tr>
+				<td class="bilgi">&nbsp;</td>
+				<td class="input"><input type="submit" value="Kaydet" /></td>
 			</tr>
 		</table>
 	</form>
