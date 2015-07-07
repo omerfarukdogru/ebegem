42d63d102dd948879c219c71eca84055acb7f81c
mustafa220 <musto_220@windowslive.com>
2015-07-07 23:18:48 +0300

    ogretmen duzenle

--- yonetim/apps/model/ogretmenDuzenle_model.php
+++ yonetim/apps/model/ogretmenDuzenle_model.php
@@ -5,6 +5,12 @@
 		}
 		public function veriler($q){
 			$data=array();
+			$data["q"]=$q;
+			$id = clk::temizle(@$q[1]);
+			if($id){
+				$data["ogretmenDetay"] = $this->select("ogretmenler","id='$id'");
+			}
+			$data["ogretmenler"] = $this->select("ogretmenler");
 			return $data;
 		}
 		public function __destruct(){
--- yonetim/apps/view/ogretmenDuzenle_view.php
+++ yonetim/apps/view/ogretmenDuzenle_view.php
@@ -1,3 +1,44 @@
 <div class="sag">
-	
+	<ul class="duyuruListe">
+	<?php
+		if(!@$q[1]){
+			foreach($ogretmenler as $ogretmen){
+			?>
+		<li><a href="<?php echo clk::site(); ?>/ogretmenDuzenle/<?php echo $ogretmen["id"]; ?>"><?php echo $ogretmen["ogretmenAdi"]; ?></a></li>
+			<?php
+			}
+		}
+		else{
+			foreach($ogretmenDetay as $ogretmen){
+			?>
+	<form action="" method="post" enctype="multipart/form-data">
+		<table class="formTablo">
+			<tr>
+				<td class="bilgi">Öğretmen Adı</td>
+				<td class="input"><input type="text" name="ogretmenAdi" value="<?php echo $ogretmen["ogretmenAdi"]; ?>" /></td>
+			</tr>
+			<tr>
+				<td class="bilgi">Öğretmen Bilgileri</td>
+				<td class="input"><textarea name="ogretmenBilgileri" cols="30" rows="10"><?php echo $ogretmen["ogretmenBilgileri"]; ?></textarea></td>
+			</tr>
+			<tr>
+				<td class="bilgi">Öğretmen Resmi</td>
+				<td class="input">
+					<div class="resimDiv2">
+						<img src="<?php echo clk::site(); ?>/../images/<?php echo $ogretmen["ogretmenResim"]; ?>" />
+					</div>
+					<input type="file" name="resim" id="resim" /> <label for="resim">Değiştirmek için seçin. </label>
+				</td>
+			</tr>
+			<tr>
+				<td class="bilgi">&nbsp;</td>
+				<td class="input"><input type="submit" value="Kaydet" /></td>
+			</tr>
+		</table>
+	</form>
+			<?php
+			}
+		}
+	?>
+	</ul>
 </div>
